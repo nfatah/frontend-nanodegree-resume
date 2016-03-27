21f0bf70aacf1c3912742cc2f3fc2842422f7382
nfatah <nfatah41@gmail.com>
2016-03-27 16:17:28 +0300

    initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var work = {};
+work.position = "Megatron";
+work.city = "Cybercity";
+work.noOfYears = 22;
+
+var eduaction = {};
+education["lastSchool"] = "StreetSchool";
+education["Years"] = 10;
+education.city = "Cybercity";
+
+$("#main").append(work["position"]);
